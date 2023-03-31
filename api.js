a51b741db41efe476d22bf8a4ba3ff1159b71052
kati1989 <kati_1989@yahoo.com>
2023-03-31 23:17:11 +0300

    sort

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ function api(path,method,body=null){
         headers:{
             'Content-Type':'application/json;charset=utf-8',
             
-        }
+        },
 
       
     };
@@ -36,15 +36,17 @@ async function getAllCars(){
 }
 
 //functie care adauga o masina
+/////nu este data un json
 
 async function addCar(car){
 
     let data= await api("add",'POST',car);
 
-    return data.json();
+    return data;
   }
 
 async function getAllMarci(){
+
     let data= await api("masini/marci",'GET');//in functia api avem http://localhost:8080/api/v1/"+path aici completam pathul mai departe de la endpoint:masini/marci
 
     data=await data.json();
@@ -53,30 +55,18 @@ async function getAllMarci(){
     return data;
 }
 
-//masini/{marca}
 
-async function getAllMasiniByMarca(marca){
-    let data=await api(`masini/${marca}`,'GET');
-    
 
-    data=await data.json();
-
-    console.log("data1:"+data);
-
-    return data;
-}
-
+//
  async function deleteCar(carId){
     let data=await api(`delete/${carId}`,'DELETE');
  }
 
- /*async function getAllFilms(){
-    let dataApi= await fetch("https://api.themoviedb.org/3/movie/popular?api_key=29c67b39adde33f44618a80626c264ec&language=en-US&page=1");   
-    dataApi=await dataApi.json();
-    console.log(dataApi);
-    attachCards(dataApi.results,".films");
-    data = dataApi.results;
-}*/
+ async function deleteCarByModel(model){
+    let data=await api(`deleteByModel/${model}`,'DELETE');
+ }
+
+
 
 async function updateCar(car){
 
@@ -88,4 +78,48 @@ async function updateCar(car){
     return data;
 }
 
+
+async function sortByMarca(){
+
+    let data=await api(`sortByMarca`,'GET');
+
+    data=await data.json();
+
+    return data;
+}
+
+async function sortByModel(){
+    let data=await api(`sortByModel`,'GET');
+
+    data = await data.json(data);
+
+    return data;
+}
+
+async function sortByCuloare(){
+     let data=await api(`sortByColor`,'GET')
+
+     data=await data.json();
+
+     return data;
+}
+
+async function sortByNrDeLocuri(){
+    let data=await api(`sortByNrDeLoc`,'GET');
+
+    data=await data.json();
+
+    return data;
+}
+
+async function sortByPret(){
+    
+    let data=await api(`sortByPrice`,'GET');
+
+     data=await  data.json();
+
+     return data;
+
+
+}
 
--- functions.js
+++ functions.js
@@ -1,13 +1,18 @@
-async function  attachHomePage(){
-    let container=document.querySelector(".container");
+async function attachHomePage() {
+  let container = document.querySelector(".container");
 
-     container.innerHTML=`   
+  container.innerHTML = `   
+
     <h1>Cars</h1>
 	<p><a class="button new-car">Create New Car</a></p>
+
+
+    
   
 	<table>
-		<thead>
-			<tr>
+    
+		<thead >
+			<tr class="container-sort">
             <th  class="id">Id</th>
             <th class="culoare">Culoare</th>
 			<th class="marca">Marca</th>
@@ -17,75 +22,104 @@ async function  attachHomePage(){
 				
 			</tr>
 		</thead>
-		<tbody class=container-masini>
+
+		<tbody class="container-masini">
 		
 		</tbody>
 	</table>
-    `
-
+    
+ 
+   
+    `;
+    //container?
     let data = await getAllCars();
     attachRows(data);
 
+let containerSort=document.querySelector(".container-sort");
 
+  containerSort.addEventListener("click", async (e) => {
+    e.preventDefault;
+    let data = e.target;
 
-let btnNewCar=document.querySelector(".new-car");
-
-
-btnNewCar.addEventListener("click",(e)=>{
-                attachNewCarPage();
-    });        
-
-    let rowsContainer=document.querySelector(".container-masini");
-
-    rowsContainer.addEventListener("click",(e)=>{
-
-        console.log("ceca");        
-     
-        let data=e.target.parentNode;
-        console.log(data);
-        
-        let carProperties= data.children;
-
-       // car.id=carProperties[0].innerHTML;
-       const car = {
-        carId : carProperties[0].innerHTML,
-        culoare : carProperties[1].innerHTML,
-        marca : carProperties[2].innerHTML,
-        model : carProperties[3].innerHTML,
-        nrDeLocuri : carProperties[4].innerHTML,
-        pret : carProperties[5].innerHTML     
+    if (data.classList.contains("marca")) {
+      let vec = await sortByMarca();
+      console.log(vec);
+      attachRows(vec);
+    } else if (data.classList.contains("model")) {
+      let vec = await sortByModel();
+      console.log(vec);
+      attachRows(vec);
+    } else if (data.classList.contains("nrDeLocuri")) {
+      let vec = await sortByNrDeLocuri();
+      console.log(vec);
+      attachRows(vec);
+    } else if (data.classList.contains("pret")) {
+      let vec = await sortByPret();
+      console.log(vec);
+      attachRows(vec);
+    } else if (data.classList.contains("culoare")) {
+      let vec = await sortByCuloare();
+      console.log(vec);
+      attachRows(vec);
     }
+  });
 
-        attachUpdatePage(car);
-    });
 
+
+  let btnAddNewCar = document.querySelector(".new-car");
+
+  btnAddNewCar.addEventListener("click", (e) => {
+    attachNewCarPage();
+  });
+
+  let rowsContainer = document.querySelector(".container-masini");
+
+  rowsContainer.addEventListener("click", (e) => {
+
+
+    e.preventDefault();
+    let data = e.target.parentNode;
+
+    let carProperties = data.children;
+
+    // car.id=carProperties[0].innerHTML;
+    const car = {
+      carId: carProperties[0].innerHTML,
+      culoare: carProperties[1].innerHTML,
+      marca: carProperties[2].innerHTML,
+      model: carProperties[3].innerHTML,
+      nrDeLocuri: carProperties[4].innerHTML,
+      pret: carProperties[5].innerHTML,
+    };
+
+    attachUpdatePage(car);
+  });
 }
 
 //cere valorile noi si face update,sare la pagina principala cu valoarea noua
-function update(){
+function update() {
+  let inp1 = document.getElementById("culoareInp");
+  let inp2 = document.getElementById("marcaInp");
+  let inp3 = document.getElementById("modelInp");
+  let inp4 = document.getElementById("nrDeLocuriInp");
+  let inp5 = document.getElementById("pretInp");
 
-     let inp1=document.getElementById('culoare');
-     let inp2=document.getElementById('marca');
-     let inp3=document.getElementById('model');
-     let inp4=document.getElementById('nrDeLocuri');
-     let inp5=document.getElementById('pret');
+  const car = {
+    culoare: inp1.value,
+    marca: inp2.value,
+    model: inp3.value,
+    nrDeLocuri: inp4.value,
+    pret: inp5.value,
+  };
 
-    const car = {
-        culoare : inp1.value,
-        marca : inp2.value,
-        model : inp3.value,
-        nrDeLocuri : inp4.value,
-        pret : inp5.value     
-    }
-
-    updateCar(car);
+  updateCar(car);
 }
 
- async function attachUpdatePage(car){
-    let container=document.querySelector(".container")
+async function attachUpdatePage(car) {
+  let container = document.querySelector(".container");
 
-    //input type=hidden nu este visibil pe pagina, dar se poate citi valoarea lui.
-    container.innerHTML=` <h1>Update car</h1> 
+  //input type=hidden nu este visibil pe pagina, dar se poate citi valoarea lui.
+  container.innerHTML = ` <h1>Update car</h1> 
         <input name="carId" class="carId" type="hidden" value="${car.carId}"/>
         
         
@@ -94,146 +128,126 @@ function update(){
         </ul>
 
         <p>
-            <label for="marca">marca</label>
+            <label for="marcaInp">marca</label>
             <input name="marca" type="text" class="marca" id="marca" value="${car.marca}"  disabled>
         </p>
         <p>
-            <label for="model">Model</label>
-            <input name="model" type="text" class="model" id="model" value="${car.model}">
+            <label for="modeInpl">Model</label>
+            <input name="modelInp" type="text" class="model" id="model" value="${car.model}">
         </p>
         <p>
-            <label for="culoare">Culoare</label>
-            <input name="culoare" type="text"  class="culoare" id="culoare" value="${car.culoare}">
+            <label for="culoareInp">Culoare</label>
+            <input name="culoareInp" type="text"  class="culoare" id="culoare" value="${car.culoare}">
         </p>
         <p>
-            <label for="nrDeLocuri">Numar de locuri</label>
-            <input name="nrDeLocuri" type="text" class="nrDeLocuri" id="nrDeLocuri" value="${car.nrDeLocuri}">
+            <label for="nrDeLocuriInp">Numar de locuri</label>
+            <input name="nrDeLocuriInp" type="text" class="nrDeLocuri" id="nrDeLocuri" value="${car.nrDeLocuri}">
         </p>
         <p>
-            <label for="pret">Pret</label>
-            <input name="pret" type="text" class="pret" id="pret" value="${car.pret}">
+            <label for="pretInp">Pret</label>
+            <input name="pretInp" type="text" class="pret" id="pret" value="${car.pret}">
         </p>
         <div>
             <button class="update">Update car</button>
             <button class="delete" >Delete car</button>
             <button class="cancel">Cancel</button>
         </div>
-  `
+  `;
 
-  let btnCancel=document.querySelector(".cancel")
-  btnCancel.addEventListener("click",()=>{
+  let btnCancel = document.querySelector(".cancel");
+  btnCancel.addEventListener("click", () => {
     attachHomePage();
-  })
+  });
 
-  let btnUpdate=document.querySelector(".update");
-  btnUpdate.addEventListener("click",async()=>{
+  let btnUpdate = document.querySelector(".update");
+  btnUpdate.addEventListener("click", async () => {
+    let input1 = document.querySelector(".marca");
+    let input2 = document.querySelector(".model");
+    let input3 = document.querySelector(".culoare");
+    let input4 = document.querySelector(".nrDeLocuri");
+    let input5 = document.querySelector(".pret");
 
-
-    let input1=document.querySelector(".marca");
-    let input2=document.querySelector(".model");
-    let input3=document.querySelector(".culoare");      
-    let input4=document.querySelector(".nrDeLocuri");
-    let input5=document.querySelector(".pret");
-
-    let car={
-        marca:input1.value,
-        model:input2.value,
-        culoare:input3.value,
-        nrDeLocuri:input4.value,
-        pret:input5.value
+    let car = {
+      marca: input1.value,
+      model: input2.value,
+      culoare: input3.value,
+      nrDeLocuri: input4.value,
+      pret: input5.value,
     };
 
-    
+    let erors = [];
 
-    let erors=[];
+    if (input2.value == "") {
+      erors.push("trebuie pusa modelul");
 
-
-    
-    if(input2.value==""){
-    
-    erors.push("trebuie pusa modelul");
-    
-
-    input2.style.borderColor="red";
-
-    }   
-
-    if(input3.value==""){
-        erors.push("trebuie pusa culoarea");
-
-        input3.style.borderColor="red";
-        
+      input2.style.borderColor = "red";
     }
 
-    if(input4.value==0){
-        erors.push("trebuie pus numarul de locuri");
-        input4.style.borderColor="red";
-    }else if(input4.value<0){
-        erors.push("Numarul de locuri nu poate fi un numar negativ");
-        input4.style.borderColor="red";
+    if (input3.value == "") {
+      erors.push("trebuie pusa culoarea");
+
+      input3.style.borderColor = "red";
     }
 
-    if(input5.value==0){
-        erors.push("trebuie pus pretul");
-        input5.style.borderColor="red";
+    if (input4.value == 0) {
+      erors.push("trebuie pus numarul de locuri");
+      input4.style.borderColor = "red";
+    } else if (input4.value < 0) {
+      erors.push("Numarul de locuri nu poate fi un numar negativ");
+      input4.style.borderColor = "red";
     }
 
-  
-   
-if (erors.length>0){
-   
-    let errorContainer=document.querySelector(".error");
+    if (input5.value == 0) {
+      erors.push("trebuie pus pretul");
+      input5.style.borderColor = "red";
+    }
 
-    let h1=document.createElement("h1");
-    h1.textContent="Ooops";
-    errorContainer.appendChild(h1);
+    if (erors.length > 0) {
+      let errorContainer = document.querySelector(".error");
 
+      let h1 = document.createElement("h1");
 
-    for(let i=0;i<erors.length;i++){
+      h1.textContent = "Ooops";
 
-        let li=document.createElement("li");
+      errorContainer.appendChild(h1);
 
-        li.textContent=erors[i];
+      for (let i = 0; i < erors.length; i++) {
+        let li = document.createElement("li");
+
+        li.textContent = erors[i];
 
         errorContainer.appendChild(li);
+      }
+    } else {
+      let errorContainer = document.querySelector(".error");
+
+      errorContainer.innerHTML = "";
     }
-}else {
-    let errorContainer=document.querySelector(".error");
+    if (erors.length == 0) {
+      let data = await updateCar(car);
 
-    errorContainer.innerHTML="";
+      attachHomePage();
+    }
+  });
 
+  let btnDelete = document.querySelector(".delete");
 
-}
-if(erors.length==0){
-   
-    let data=await updateCar(car);
+  btnDelete.addEventListener("click", async () => {
+    let input = document.querySelector(".carId");
+
+    let carId = input.value;
+
+    let data = await deleteCar(carId);
+
     attachHomePage();
+  });
 }
 
+//sa navigheze pe  new car
+function attachNewCarPage() {
+  let container = document.querySelector(".container");
 
-
-
-  })
-
-  let btnDelete=document.querySelector(".delete");
-
-    btnDelete.addEventListener("click",async()=>{
-        let input = document.querySelector(".carId");
-
-        let carId=input.value;
-
-        let data=await deleteCar(carId);
-
-        attachHomePage();
-    });
- }
-
-
- //sa navigheze pe  new car
-function attachNewCarPage(){
-
-    let container=document.querySelector(".container");
-    container.innerHTML=`
+  container.innerHTML = `
 
     
     <h1>New Car</h1>
@@ -242,156 +256,149 @@ function attachNewCarPage(){
             
     </ul>
         <p>
-            <label for="year">Culoare</label>
-            <input name="year" type="text" id="culoare" class="culoare">
+            <label for="culoare">Culoare</label>
+            <input name="culoare" type="text" id="culoare" class="culoare">
         </p>
         <p>
             <label for="title">Marca</label>
-            <input name="title" type="text" id="marca" class="marca">
+            <input name="marca" type="text" id="marca" class="marca">
         </p>
         <p>
-            <label for="author">Model</label>
-            <input name="author" type="text" id="model" class="model">
+            <label for="model">Model</label>
+            <input name="model" type="text" id="model" class="model">
         </p>
         
         <p>
-            <label for="year">NrDeLocuri</label>
-            <input name="year" type="text"  class="nrDeLocuri" id="nrDeLocuri">
+            <label for="nrdeLocuri">NrDeLocuri</label>
+            <input name="brDeLocuri" type="text"  class="nrDeLocuri" id="nrDeLocuri">
         </p>
 
         <p>
-            <label for="year">pret</label>
-            <input name="year" type="text" id="pret" class="pret">
+            <label for="pret">pret</label>
+            <input name="pret" type="text" id="pret" class="pret">
         </p>
 
         <div class="butoane">
             <button class="add">Add new  Car</button>
             <button class="cancel">Cancel</button>
             </div
-    `
+    `;
 
-    let btnCancel=document.querySelector(".cancel")
-    btnCancel.addEventListener("click",()=>{
-      attachHomePage();
-    })
+  let btnCancel = document.querySelector(".cancel");
+  btnCancel.addEventListener("click", () => {
+    attachHomePage();
+  });
 
-     let btnAddNewCar=document.querySelector(".add");
-     let inp0=document.getElementById('carId');
-     let inp1=document.getElementById('culoare');
-     let inp2=document.getElementById('marca');
-     let inp3=document.getElementById('model');
-     let inp4=document.getElementById('nrDeLocuri');
-     let inp5=document.getElementById('pret');
+  let btnAddNewCar = document.querySelector(".add");
+  let inp0 = document.getElementById("carId");
+  let inp1 = document.getElementById("culoare");
+  let inp2 = document.getElementById("marca");
+  let inp3 = document.getElementById("model");
+  let inp4 = document.getElementById("nrDeLocuri");
+  let inp5 = document.getElementById("pret");
 
-     btnAddNewCar.addEventListener("click", async ()=>{
-        
-    let inp1=document.querySelector(".culoare");
-    let inp2=document.querySelector(".marca");
-    let inp3=document.querySelector(".model");
-    let inp4=document.querySelector(".nrDeLocuri");
-    let inp5=document.querySelector(".pret");
-        
-        let car={
-            culoare:inp1.value,
-            marca:inp2.value,
-            model:inp3.value,
-            nrDeLocuri:inp4.value,
-            pret:inp5.value,
-        }
-   
-    let erors=[];
-    if(inp1.value==""&& inp2.value==""&& inp3.value==""&& inp4.value==0 && inp5.value==0){
-        erors.push(" Nu ati completat campurile ");
-      
+  btnAddNewCar.addEventListener("click", async () => {
+    let inp1 = document.querySelector(".culoare");
+    let inp2 = document.querySelector(".marca");
+    let inp3 = document.querySelector(".model");
+    let inp4 = document.querySelector(".nrDeLocuri");
+    let inp5 = document.querySelector(".pret");
+
+    let car = {
+      culoare: inp1.value,
+      marca: inp2.value,
+      model: inp3.value,
+      nrDeLocuri: inp4.value,
+      pret: inp5.value,
+    };
+
+    let erors = [];
+    if (
+      inp1.value == "" &&
+      inp2.value == "" &&
+      inp3.value == "" &&
+      inp4.value == 0 &&
+      inp5.value == 0
+    ) {
+      erors.push(" Nu ati completat campurile ");
     }
 
-    if(inp1.value==""){
-        
-        erors.push("trebuie pusa culoarea");
-        
+    if (inp1.value == "") {
+      erors.push("trebuie pusa culoarea");
 
-        inp1.style.borderColor="red";
+      inp1.style.borderColor = "red";
+    }
 
-        }   
-        
-        if(inp2.value==""){
-        
-        erors.push("trebuie pusa marca");
-        
+    if (inp2.value == "") {
+      erors.push("trebuie pusa marca");
 
-        inp2.style.borderColor="red";
+      inp2.style.borderColor = "red";
+    }
 
-        }   
+    if (inp3.value == "") {
+      erors.push("trebuie pusa model");
 
-        if(inp3.value==""){
-            erors.push("trebuie pusa model");
+      inp3.style.borderColor = "red";
+    }
 
-            inp3.style.borderColor="red";
-            
-        }
+    if (inp4.value == 0) {
+      erors.push("trebuie pus numarul de locuri");
 
-        if(inp4.value==0){
-            erors.push("trebuie pus numarul de locuri");
-            inp4.style.borderColor="red";
-        }
+      inp4.style.borderColor = "red";
+    }
 
-        if(inp5.value==0){
-            erors.push("trebuie pus pretul");
-            inp5.style.borderColor="red";
-        }
+    if (inp5.value == 0) {
+      erors.push("trebuie pus pretul");
 
-        if (erors.length>0){
-        
-            let errorContainer=document.querySelector(".error");
+      inp5.style.borderColor = "red";
+    }
 
-            errorContainer.innerHTML="";
-            let h1=document.createElement("h1");
-            h1.textContent="Ooops";
-            errorContainer.appendChild(h1);
-            
+    if (erors.length > 0) {
+      let errorContainer = document.querySelector(".error");
 
+      errorContainer.innerHTML = "";
 
-            for(let i=0;i<erors.length;i++){
+      let h1 = document.createElement("h1");
 
-                let li=document.createElement("li");
+      h1.textContent = "Ooops";
 
-                li.textContent=erors[i];
+      errorContainer.appendChild(h1);
 
-                errorContainer.appendChild(li);
-            }
-        } else {
-           
-           
-         let data=await addCar(car);
-         attachHomePage();
-        }
-    })
+      for (let i = 0; i < erors.length; i++) {
+        let li = document.createElement("li");
 
+        li.textContent = erors[i];
+
+        errorContainer.appendChild(li);
+      }
+    } else {
+      let data = await addCar(car);
+      attachHomePage();
+    }
+  });
 }
 
-
 //create  row
-function createRow(car){
- let tr=document.createElement("tr");
+function createRow(car) {
+  let tr = document.createElement("tr");
 
- tr.innerHTML=`
+  tr.innerHTML = `
 				<td>${car.id}</td>
                 <th>${car.culoare}</th>
 				<td>${car.marca}</td>
                 <td>${car.model}</td>
 				<td>${car.nrDeLocuri}</td>
 				<td>${car.pret}</td>	
- `
- return tr;
+ `;
+  return tr;
 }
-
 
 //attatch rows
-function attachRows(arr){
-    let container=document.querySelector(".container-masini");
-    for(let i=0;i<arr.length;i++){
-        container.appendChild(createRow(arr[i]));
-    }
+function attachRows(arr) {
+  let container = document.querySelector(".container-masini");
+
+  container.innerHTML="";
+  for (let i = 0; i < arr.length; i++) {
+    container.appendChild(createRow(arr[i]));
+  }
 }
-
-
